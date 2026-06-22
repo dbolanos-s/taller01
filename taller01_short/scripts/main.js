0b87cdaf9a8a907715415c0d78f2ec9e84a717e6
Doménica Bolaños <dbolanos@espol.edu.ec>
2026-06-22 19:32:53 +0000

    taller01 completado

--- taller01_short/scripts/main.js
+++ taller01_short/scripts/main.js
@@ -1,26 +1,45 @@
-// Plantilla para cada canción:
-let card_song = `<article class="song-card">
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    cargarCanciones();
+});
+
+const cargarCanciones = async () => {
+    try {
+        const response = await fetch(
+            "https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Youtube/only_songs.json"
+        );
+
+        const data = await response.json();
+
+        const canciones = data.slice(0, 12);
+
+        let formatter = new Intl.NumberFormat('en-US');
+
+        const songsGrid = document.querySelector(".songs-grid");
+
+        canciones.forEach((song) => {
+
+            let card_song = `<article class="song-card">
     <div class="cover">
-        <img src="https://i.ytimg.com/vi/V9PVRfjEBTI/maxresdefault.jpg"
-            alt="Portada: Billie Eilish - BIRDS OF A FEATHER (Official Music Video)">
-        <span class="badge">3:51:00</span>
+        <img src="${song.thumbnail}"
+            alt="Portada: ${song.title}">
+        <span class="badge">${song.duration_string}</span>
     </div>
     <div class="content">
-        <h2 class="title">Billie Eilish - BIRDS OF A FEATHER (Official Music Video)</h2>
-        <div class="meta">558,329,099 vistas</div>
+        <h2 class="title">${song.title}</h2>
+        <div class="meta">${formatter.format(song.view_count)} vistas</div>
         <div class="footer">
-            <span class="channel">Canal: <a href="https://www.youtube.com/channel/UCDGmojLIoWpXok597xYo8cg"
-                    target="_blank" rel="noopener noreferrer">Billie Eilish</a></span>
+            <span class="channel">Canal: <a href="${song.channel_url}"
+                    target="_blank" rel="noopener noreferrer">${song.channel}</a></span>
         </div>
     </div>
-</article>`
+</article>`;
 
-// Formateador de números
-let formatter = new Intl.NumberFormat('en-US');
+            songsGrid.innerHTML += card_song;
+        });
 
-// Ejemplo de uso:
-// let views_witoutformat = 2536628;
-
-
-// let views_withformat= formatter.format(views_witoutformat); 
-// Valor de views_withformat: "2,536,628"
+    } catch (error) {
+        alert("Error al cargar los datos: " + error.message);
+    }
+};
